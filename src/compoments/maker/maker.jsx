c257c6e92cdfd5072fc5db74c79bca6805f7cccb
SeungYeong <lsyeong93@naver.com>
2021-04-02 22:58:30 +0900

    state 추가, 삭제 핸들러 작업

--- src/compoments/maker/maker.jsx
+++ src/compoments/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    '1': {
       id: '1',
       name: 'SeungYeong',
       company: 'null',
@@ -19,7 +19,8 @@ const Maker = ({ authService }) => {
       fileName: 'seung',
       fileURL: null,
     },
-    {
+
+    '2': {
       id: '2',
       name: 'SeungYeong',
       company: 'null',
@@ -30,7 +31,8 @@ const Maker = ({ authService }) => {
       fileName: 'seung',
       fileURL: 'seung.png',
     },
-    {
+
+    '3': {
       id: '3',
       name: 'SeungYeong',
       company: 'null',
@@ -41,7 +43,8 @@ const Maker = ({ authService }) => {
       fileName: 'seung',
       fileURL: null,
     },
-  ]);
+  });
+
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -55,15 +58,30 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const CreateOrupdateCard = card => {
+    setCards(cards => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+
+
   }
+
+  const deleteCard = card => {
+    setCards(cards => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
+
+  }
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor cards={cards} addCard={CreateOrupdateCard} updateCard={CreateOrupdateCard} deleteCard={deleteCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
