7bf33281ed9cb0a15ac1d9c63d2eed20e0d5f1a1
SeungYeong <lsyeong93@naver.com>
2021-04-12 13:09:10 +0900

    성능정검: memo 추가

--- src/compoments/header/header.jsx
+++ src/compoments/header/header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './header.module.css';
 
-const Header = ({ onLogout }) => (
+const Header = memo(({ onLogout }) => (
   <header className={styles.header}>
     {onLogout && (<button onClick={onLogout} className={styles.logout}>
       Logout
@@ -10,6 +10,6 @@ const Header = ({ onLogout }) => (
     <h1 className={styles.title}>Business Card Maker</h1>
   </header>
 
-);
+));
 
 export default Header;
